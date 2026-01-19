041e64561581baa1d7a0e0ff8b2e4a4e2cac2cb4
DESKTOP-OGD4MGE\kyong <kyonglok@nate.com>
2026-01-19 14:18:12 +0900

    Initial Commit: Setup Fabric 1.21.11 project

---
+++ src/client/java/com/liante/client/MingtdClient.java
@@ -0,0 +1,10 @@
+package com.liante.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MingtdClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/liante/Mingtd.java
@@ -0,0 +1,10 @@
+package com.liante;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mingtd implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
